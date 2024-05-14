a0055f652160b8be8ea4b26b5913140468bb1271
chralis <eiju.chinen@gmail.com>
2024-05-14 12:25:36 +0900

    Update astro_config set site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://pranariaspage.onrender.com',
   integrations: [mdx(), sitemap(), tailwind()],
   server: { port:8000 }
 });
